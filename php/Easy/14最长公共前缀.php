3d20f51fcaf64bbd47dd765a45b882dcf7bbea07
tony <3546544379@qq.com>
2020-09-01 11:29:04 +0800

    14

--- php/Easy/14最长公共前缀.php
+++ php/Easy/14最长公共前缀.php
@@ -38,4 +38,39 @@ class Solution {
 
 	}
 
+	/**
+	 * 标签：滑动窗口
+	 * 当字符串数组长度为 0 时则公共前缀为空，直接返回
+	 * 令最长公共前缀 ans 的值为第一个字符串，进行初始化
+	 * 遍历后面的字符串，依次将其与 ans 进行比较，两两找出公共前缀，最终结果即为最长公共前缀
+	 * 如果查找过程中出现了 ans 为空的情况，则公共前缀不存在直接返回
+	 * 时间复杂度：O(s)O(s)，s 为所有字符串的长度之和
+	 */
+	function two($strs) {
+		$len = count($strs);
+		if ($len == 0) {
+			return '';
+		}
+
+		$ans = $strs[0];
+		for ($i=1; $i<$len; $i++) {
+			$j = 0;
+			// j为公共前缀的长度
+			for (; $j < strlen($ans) && $j < strlen($strs[$i]); $j++) {
+				if ($ans[$j] != $strs[$i][$j]) {
+					break;
+				}
+			}
+			$ans = substr($ans, 0, $j);
+			if ($ans == '') {
+				return $ans;
+			}
+		}
+
+		return $ans;
+    }
+
 }
+
+$str = ["flower","flow","flight"];
+
