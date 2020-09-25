a5a2e7cac523050f436ee973f750bdfc62dcbd0f
tony <3546544379@qq.com>
2020-09-25 09:51:49 +0800

    501.二叉搜索树中的众数,617.合并二叉树

--- php/Easy/501.二叉搜索树中的众数.php
+++ php/Easy/501.二叉搜索树中的众数.php
@@ -5,4 +5,81 @@
  * @author   yangshuo
  * @date     2020/9/24
  */
- 
+
+/**
+ * 给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+
+	假定 BST 有如下定义：
+
+	结点左子树中所含结点的值小于等于当前结点的值
+	结点右子树中所含结点的值大于等于当前结点的值
+	左子树和右子树都是二叉搜索树
+	例如：
+	给定 BST [1,null,2,2],
+	
+	1
+     \
+     2
+	/
+   2
+	返回[2].
+
+	提示：如果众数超过1个，不需考虑输出顺序
+
+	进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内）
+
+
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+	/**
+	 * @param TreeNode $root
+	 * @return Integer[]
+	 */
+	function findMode($root) {
+		if (empty($root)) {
+			return [];
+		}
+
+		$stack = [];
+		$count = 0;
+		$maxCount = 1;
+		$base = null;
+		$res = [];
+
+		while (!empty($stack) || !empty($root)) {
+			if (!empty($root)) {
+				$stack[] = $root;
+				$root = $root->left;
+			} else {
+				$root = array_pop($stack);
+				$val = $root->val;
+				if ($val == $base) {
+					++$count;
+				} else {
+					$count = 1;
+					$base = $val;
+				}
+				if ($count == $maxCount) {
+					$res[] = $val;
+				}
+
+				if ($count > $maxCount) {
+					$maxCount = $count;
+					$res = null;
+					$res[] = $val;
+				}
+				$root = $root->right;
+			}
+		}
+
+		return $res;
+	}
+}
--- php/Easy/617.合并二叉树.php
+++ php/Easy/617.合并二叉树.php
@@ -5,4 +5,36 @@
  * @author   yangshuo
  * @date     2020/9/23
  */
- 
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+	/**
+	 * @param TreeNode $t1
+	 * @param TreeNode $t2
+	 * @return TreeNode
+	 */
+	function mergeTrees($t1, $t2) {
+		if (empty($t1)) {
+			return $t2;
+		}
+
+		if (empty($t2)) {
+			return $t1;
+		}
+
+		$merge = new TreeNode($t1->val + $t2->val);
+		$merge->left = $this->mergeTrees($t1->left, $t2->left);
+		$merge->right = $this->mergeTrees($t1->right, $t2->right);
+
+		return $merge;
+	}
+}
