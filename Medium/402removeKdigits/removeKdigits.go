edd553a7515d3ac7fd9c5f3b90979a6651969936
yangshuo07 <yangshuo07@zuoyebang.com>
2022-04-15 17:51:50 +0800

    <402移除k位数字> fix case:[0200]

---
+++ Medium/402removeKdigits/removeKdigits.go
@@ -0,0 +1,39 @@
+package _02removeKdigits
+/**
+  移除k位数字，成为最小
+  https://leetcode-cn.com/problems/remove-k-digits/
+  贪心、单调栈
+*/
+
+import "strings"
+
+
+func removeKdigits(num string, k int) string {
+	n := len(num)
+	if n <= k {
+		return "0"
+	}
+
+	s := []byte{}
+	timer := 0
+
+	for i := range num {
+		v := num[i]
+
+		if i < n-1 && num[i+1] < v && timer < k {
+			timer++
+			continue
+		}
+
+		s = append(s, v)
+	}
+
+	res := strings.TrimLeft(string(s), "0")
+	if res == "" {
+		return "0"
+	}
+
+	return res
+}
+
+
